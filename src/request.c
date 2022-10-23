b4dd9e69c5eee09a656fa65bc0b6203ba879f858
Dino Korah <dino.korah@redmatter.com>
2022-10-23 18:36:19 +0000

    Convert version definitions to integer

--- src/request.c
+++ src/request.c
@@ -1511,7 +1511,7 @@ S3Status request_api_initialize(const char *userAgentInfo, int flags,
     }
 
     snprintf(userAgentG, sizeof(userAgentG),
-             "Mozilla/4.0 (Compatible; %s; libs3 %s.%s; %s)",
+             "Mozilla/4.0 (Compatible; %s; libs3 %d.%d; %s)",
              userAgentInfo, LIBS3_VER_MAJOR, LIBS3_VER_MINOR, platform);
 
     xmlInitParser();
